e963c6da6b3386c91dd24ab21466731797611453
zeads <ruri.pelinandang@gmail.com>
2026-04-08 07:59:36 +0700

    feat: build main livewire layout

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>
+            {{ isset($title) ? "$title - " . config('app.name') : config('app.name') }}
+        </title>
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+        <link rel="manifest" href="/site.webmanifest">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @stack('head')
+
+        {{-- @livewireStyles --}}
+    </head>
+    <body>
+        <x-navigation />
+        {{ $slot }}
+
+        <x-footer />
+        {{-- @livewireScripts --}}
+    </body>
+</html>
