e86dec35d46fd5e03fec107d86159063d3ca2f59
zeads <ruri.pelinandang@gmail.com>
2026-04-10 07:43:45 +0700

    feat: add Product Catalog Indicator Loading in Apply button

--- resources/views/livewire/product-catalog.blade.php
+++ resources/views/livewire/product-catalog.blade.php
@@ -52,8 +52,12 @@
                 <div class="grid grid-cols-2 mt-10">
                     <button type="button"
                         wire:click="applyFilter"
+                        wire:loading.attr="disabled"
                         class="inline-flex items-center justify-center px-4 py-3 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-lg cursor-pointer gap-x-2 hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
                         Apply Filter
+                        <div wire:loading class="animate-spin inline-block size-4 border-3 border-current border-t-transparent rounded-[999px] text-primary" role="status" aria-label="loading">
+                            <span class="sr-only">Loading...</span>
+                        </div>
                     </button>
                     <button type="button"
                         wire:click="resetFilter"
