3791c4cab16e2358b4b1e2aaa7a0cef560bc4fd3
zeads <ruri.pelinandang@gmail.com>
2026-04-13 17:33:34 +0700

    feat: Make a cart system service

---
+++ app/Services/SessionCartService.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Contract\CartServiceInterface;
+use App\Data\CartData;
+use App\Data\CartItemData;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Session;
+use Spatie\LaravelData\DataCollection;
+
+class SessionCartService implements CartServiceInterface
+{
+    protected string $sessionKey = 'cart';
+
+    protected function load() : DataCollection
+    {
+        $raw = Session::get($this->sessionKey, []);
+
+        return new DataCollection(CartItemData::class, $raw);
+
+    }
+
+    /** @param Collection<int, CartItemData> */
+    protected function save(Collection $items) : void
+    {
+        Session::put($this->sessionKey, $items->values()->all());
+    }
+
+    public function addOrUpdate(CartItemData $item) : void
+    {
+        // 1. Tarik Data
+        $collection = $this->load()->toCollection();
+        $updated = false;
+
+        // 2. Mapping
+        $cart = $collection->map(function(CartItemData $i) use ($item, &$updated) {
+            if($i->sku === $item->sku) {
+                $updated = true;
+                return $item;
+            }
+
+            return $i;
+        })->values()->collect();
+
+        if(! $updated) {
+            $cart->push($item);
+        }
+
+        // 3. Save
+        $this->save($cart);
+
+    }
+
+    public function remove(string $sku) : void
+    {
+        $cart = $this->load()->toCollection()
+        ->reject(fn(CartItemData $i) => $i->sku === $sku)
+        ->values()
+        ->collect();
+
+        $this->save($cart);
+    }
+
+    public function getItemBySku(string $sku) : ?CartItemData
+    {
+        return $this->load()->toCollection()->first(fn(CartItemData $i) => $i->sku === $sku);
+    }
+
+    public function all() : CartData
+    {
+        return new CartData($this->load());
+    }
+}
