4246ad97f1487c76eccafc606d7b5c42d48777c8
zeads <ruri.pelinandang@gmail.com>
2026-04-22 07:15:28 +0700

    feat: Make a Payment Driver Interface

---
+++ app/Data/PaymentData.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Data;
+
+use Spatie\LaravelData\Attributes\Computed;
+use Spatie\LaravelData\Data;
+
+class PaymentData extends Data
+{
+    #[Computed]
+    public string $hash;
+
+
+    public function __construct(
+        public string $driver,
+        public string $method,
+        public string $label,
+        public string $payload = []
+    ) {
+        $this->hash = md5("{$driver}|{$method}|". json_encode($payload));
+    }
+}
