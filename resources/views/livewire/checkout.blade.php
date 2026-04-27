d3704ca8c6e0d6d5b0422dd1e27d5c96e5275f6b
zeads <ruri.pelinandang@gmail.com>
2026-04-28 06:40:40 +0700

    feat: Loading Indicator Place an Order

--- resources/views/livewire/checkout.blade.php
+++ resources/views/livewire/checkout.blade.php
@@ -234,9 +234,14 @@
                         </li>
                     </ul>
                     <!-- End List Group -->
-                    <button type="button" wire:click="placeAnOrder()"
+                    <button type="button"
+                        wire:loading.attr="disabled"
+                        wire:click="placeAnOrder()"
                         class="inline-flex items-center justify-center w-full px-3 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-lg gap-x-2 hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
                         Place an Order
+                        <div wire:loading class="animate-spin inline-block size-4 border-3 border-current border-t-transparent rounded-[999px] text-primary" role="status" aria-label="loading">
+                            <span class="sr-only">Loading...</span>
+                        </div>
                     </button>
                 </div>
             </div>
