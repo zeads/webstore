b0c61e0506a25afdd409f00dba1d57e3f5724c31
zeads <ruri.pelinandang@gmail.com>
2026-05-03 19:42:24 +0700

    feat: Build Mail Sales Order Created

---
+++ app/Mail/SalesOrderCreatedMail.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Mail;
+
+use App\Data\SalesOrderData;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SalesOrderCreatedMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        public SalesOrderData $sales_order
+    )
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Halo, {$this->sales_order->customer->full_name} Pesanan Nomor #{$this->sales_order->trx_id} Sudah Kami Terima',
+            to: $this->sales_order->customer->email
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.orders.created',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
