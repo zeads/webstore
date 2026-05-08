36142e30de85cc77dfcb39eb0b8009605032f095
zeads <ruri.pelinandang@gmail.com>
2026-05-09 06:19:42 +0700

    feat: Install Filament Breezy

--- CLAUDE.md
+++ CLAUDE.md
@@ -21,6 +21,7 @@ This application is a Laravel application and its main Laravel ecosystems packag
 - pestphp/pest (PEST) - v4
 - phpunit/phpunit (PHPUNIT) - v12
 - alpinejs (ALPINEJS) - v3
+- laravel-echo (ECHO) - v2
 - tailwindcss (TAILWINDCSS) - v4
 
 ## Skills Activation
--- app/Providers/Filament/BackPanelProvider.php
+++ app/Providers/Filament/BackPanelProvider.php
@@ -17,9 +17,23 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Jeffgreco13\FilamentBreezy\BreezyCore;
+
+use Livewire\Livewire;
+use Jeffgreco13\FilamentBreezy\Livewire\PersonalInfo;
+
 
 class BackPanelProvider extends PanelProvider
 {
+
+    public function boot(): void
+    {
+        // Paksa Livewire mengenali alias 'personal_info' ke class aslinya
+        Livewire::component('personal_info', PersonalInfo::class);
+    }
+
+
+
     public function panel(Panel $panel): Panel
     {
         return $panel
@@ -53,6 +67,17 @@ class BackPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                BreezyCore::make()
+                ->myProfile(
+                    shouldRegisterUserMenu: true, // Sets the 'account' link in the panel User Menu (default = true)
+                    userMenuLabel: 'My Profile', // Customizes the 'account' link label in the panel User Menu (default = null)
+                    shouldRegisterNavigation: false, // Adds a main navigation item for the My Profile page (default = false)
+                    navigationGroup: 'Settings', // Sets the navigation group for the My Profile page (default = null)
+                    hasAvatars: false, // Enables the avatar upload form component (default = false)
+                    slug: 'my-profile' // Sets the slug for the profile page (default = 'my-profile')
+                )
             ]);
     }
 }
