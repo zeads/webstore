53499639100de39652fb14cf27ead83224f4672f
zeads <ruri.pelinandang@gmail.com>
2026-05-14 06:40:49 +0700

    feat: Static Page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProductController;
 use App\Livewire\Cart;
 use App\Livewire\Checkout;
 use App\Livewire\HomePage;
+use App\Livewire\PageStatic;
 use App\Livewire\ProductCatalog;
 use App\Livewire\SalesOrderDetail;
 use App\Mail\SalesOrderCancelledMail;
@@ -23,15 +24,15 @@ Route::get('/product/{product:slug}', [ProductController::class, 'show'])->name(
 Route::get('/cart', Cart::class)->name('cart');
 Route::get('/checkout', Checkout::class)->name('checkout');
 Route::get('/order-confirmed/{sales_order:trx_id}', SalesOrderDetail::class)->name('order-confirmed');
-Route::view('/page', 'pages.page')->name('page');
+Route::get('/page/{page:slug?}', PageStatic::class)->name('page');
 
-Route::get('/mailable', function () {
+// Route::get('/mailable', function () {
 
-    return new ShippingReceiptNumberUpdatedMail(
-        SalesOrderData::from(
-            SalesOrder::latest()->first()
-        )
-    );
-});
+//     return new ShippingReceiptNumberUpdatedMail(
+//         SalesOrderData::from(
+//             SalesOrder::latest()->first()
+//         )
+//     );
+// });
 
 Route::webhooks('moota/callback');
